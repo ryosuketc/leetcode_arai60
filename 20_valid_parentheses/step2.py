6d3b67da92c34a7cf551db33213fe2146ccc4959
ryosuketc <43229670+ryosuketc@users.noreply.github.com>
2025-05-27 10:03:59 +0900

    Recover step2-2 from LeetCode submission logs

--- 20_valid_parentheses/step2.py
+++ 20_valid_parentheses/step2.py
@@ -18,3 +18,23 @@ class Solution:
         return not unmatched_open_brackets
 
 # step2-2
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Closing -> Opening brackets map.
+        closing_to_opening_brackets = {")": "(", "}": "{", "]": "["}
+        unmatched_open_brackets = []
+
+        for bracket in s:
+            is_opening = bracket in closing_to_opening_brackets.values()
+            if is_opening:
+                unmatched_open_brackets.append(bracket)
+                continue
+            
+            if not unmatched_open_brackets:
+                return False
+            actual = unmatched_open_brackets.pop()
+            expected = closing_to_opening_brackets[bracket]
+            if actual != expected:
+                return False
+
+        return not unmatched_open_brackets
