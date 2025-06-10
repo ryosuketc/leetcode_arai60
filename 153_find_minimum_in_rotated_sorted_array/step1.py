feab7aadd326b3ac485dccf173966c86d6a47d61
ryosuketc <43229670+ryosuketc@users.noreply.github.com>
2025-06-10 12:54:14 +0900

    153. Find Minimum in Rotated Sorted Array
    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/

--- 153_find_minimum_in_rotated_sorted_array/step1.py
+++ 153_find_minimum_in_rotated_sorted_array/step1.py
@@ -1,5 +1,5 @@
 # WA
-class Solution:
+class Solution1:
     def findMin(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
@@ -13,8 +13,8 @@ class Solution:
                 right = mid
         return nums[left] # == right
 
-# WA (自身が最小のときの処理を追加)
-class Solution:
+# WA (rotate されていなかったときの処理)
+class Solution2:
     def findMin(self, nums: List[int]) -> int:
         if nums[0] < nums[-1]:
             return nums[0]
@@ -32,7 +32,7 @@ class Solution:
         return nums[left] # == right
 
 # AC (`if nums[0] <= nums[mid]:` 等号を含める形に変更。mid = 0 になったときに処理できない)
-class Solution:
+class Solution3:
     def findMin(self, nums: List[int]) -> int:
         if nums[0] < nums[-1]:
             return nums[0]
@@ -47,4 +47,19 @@ class Solution:
             # mid は崖を降りた後 -> 最小値は mid またはその左にあるはず (崖を降りた直後の可能性があるので、mid 自体も最小になるかもしれない)
             else:
                 right = mid
+        return nums[left] # == right
+
+ # nums[-1] との比較。
+ class Solution4:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            # mid は崖を降りた後 -> 最小値は mid またはその左にあるはず (崖を降りた直後の可能性があるので、mid 自体も最小になるかもしれない)
+            if nums[mid] <= nums[-1]:
+                right = mid
+            # mid は崖登りの途中 -> 最小値は mid より右にあるはずで、mid 自体は最小にならない
+            else:
+                left = mid + 1
         return nums[left] # == right
