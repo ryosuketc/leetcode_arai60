f64cd454bf5786b106af5b1df9e45a1a9f1bc83b
ryosuketc <43229670+ryosuketc@users.noreply.github.com>
2025-06-25 11:07:19 +0900

    209. Minimum Size Subarray Sum
    https://leetcode.com/problems/minimum-size-subarray-sum/

--- 209_minimum_size_subarray_sum/step1.py
+++ 209_minimum_size_subarray_sum/step1.py
@@ -1,4 +1,4 @@
-from math import inf, isinf
+from math import inf
 
 class SolutionWA:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
@@ -16,4 +16,20 @@ class SolutionWA:
             return 0
         return min_len
             
-# [1, 2, 3] target=
+
+from math import
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        min_len = inf
+        left = 0
+        running_sum = 0
+        for right in range(len(nums)):
+            running_sum += nums[right]
+            while running_sum >= target:
+                min_len = min (min_len, right -left + 1)
+                running_sum -= nums[left]
+                left += 1
+        if min_len == inf:
+            return 0
+        return min_len
